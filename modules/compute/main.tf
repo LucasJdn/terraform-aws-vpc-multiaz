1f2332527be7a62a44cf208b1c7f8c1c5c38ba4e
LucasJdn <lucasjdniyczuk2018@gmail.com>
2026-08-20 17:25:50 -0300

    Add IAM role and instance profile resources for SSM session management

--- modules/compute/main.tf
+++ modules/compute/main.tf
@@ -1,3 +1,7 @@
+#----------------------------------------
+# Application Load Balancer
+#----------------------------------------
+
 resource "aws_lb_target_group" "TG_ALB"{
     name = "tg-alb"
     port = 80
@@ -35,4 +39,34 @@ resource "aws_lb_listener" "Listener_ALB"{
       type = "forward"
       target_group_arn = aws_lb_target_group.TG_ALB.arn
     }
+}
+
+#----------------------------------------
+# IAM ROLE + INSTANCE PROFILE (PRO SSM SESSION MANER)
+#----------------------------------------
+resource "aws_iam_role" "iam_role" {
+  name = "Iam-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "iam_attachment" {
+  role       = aws_iam_role.iam_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
+resource "aws_iam_instance_profile" "algum_nome" {
+  name = "project-profile"
+  role = aws_iam_role.iam_role.name
 }
