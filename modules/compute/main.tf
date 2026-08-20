d1c3ca7cee981336560ee02de84c5c12add7af61
LucasJdn <lucasjdniyczuk2018@gmail.com>
2026-08-20 17:14:09 -0300

    Add ALB and target group resources with health check configuration

--- modules/compute/main.tf
+++ modules/compute/main.tf
@@ -1,6 +1,38 @@
+resource "aws_lb_target_group" "TG_ALB"{
+    name = "tg-alb"
+    port = 80
+    protocol = "HTTP"
+    vpc_id = var.vpc_id
+
+
+    health_check {
+        path                = "/"
+        healthy_threshold   = 2
+        unhealthy_threshold = 2
+        interval            = 30
+        timeout             = 5
+  }
+}
+
 resource "aws_alb" "ALB"{
     name = "ALB"
     load_balancer_type = "application"
-    security_groups = var.alb_security_group_id.id
-    subnets = var.public_subnets_ids.id
+    internal = false
+    security_groups = [var.alb_security_group_id]
+    subnets = var.public_subnets_ids
+
+    tags = {
+        Name = "Application Load Balancer"
+    }
+}
+
+resource "aws_lb_listener" "Listener_ALB"{
+    load_balancer_arn = aws_alb.ALB.arn
+    port = 80
+    protocol = "HTTP"
+
+    default_action {
+      type = "forward"
+      target_group_arn = aws_lb_target_group.TG_ALB.arn
+    }
 }
